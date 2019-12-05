bb7c5ea2d1ea766185cecf8fc7cd43cbc7d60e1e
yang <y.shi.guo@gmail.com>
2019-12-05 11:30:40 +0800

    项目初始化

---
+++ src/exceptions/JsonException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by yang
+ * User: bonzaphp@gmail.com
+ * Date: 2019-12-05
+ * Time: 10:25
+ */
+
+namespace bonza\leads\exception;
+
+use UnexpectedValueException;
+
+class JsonException extends UnexpectedValueException
+{
+
+}
